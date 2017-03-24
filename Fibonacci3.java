9b8460731bbac6cac2f3c9a97a54132b53b46f4d
Gabriel Campos <gcampos@id.uff.br>
2017-03-24 20:06:28 -0300

    Create Fibonacci3.java

---
+++ Fibonacci3.java
@@ -0,0 +1,33 @@
+package Fib2;
+
+
+public class Fibonacci {
+    
+     static long fibo(int n) {
+        int F = 0;     // atual
+        int ant = 0;   // anterior
+ 
+        for (int i = 1; i <= n; i++) {
+ 
+            if (i == 1) {
+                F = 1;
+                ant = 0;
+            } else {
+                F += ant;
+                ant = F - ant;
+            }
+ 
+        }
+ 
+        return F;
+    }
+ 
+    public static void main(String[] args) {
+            
+           //INSIRA POSIÇÃO SOLICITA ABAIXO:
+           long tempoInicio = System.currentTimeMillis();
+            System.out.println(Fibonacci.fibo(350));
+           System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
+        }
+ 
+    }
